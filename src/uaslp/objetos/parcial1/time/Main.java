434e746be02fe84083c2760148451ad32ec4afbc
KevinVitales11 <98296858+KevinVitales11@users.noreply.github.com>
2022-02-11 13:55:34 -0600

    first commit

---
+++ src/uaslp/objetos/parcial1/time/Main.java
@@ -0,0 +1,15 @@
+package uaslp.objetos.parcial1.time;
+
+import java.sql.Time;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Time time = new Time(10,123,63);
+
+        System.out.println(time.toString());
+
+
+    }
+}
